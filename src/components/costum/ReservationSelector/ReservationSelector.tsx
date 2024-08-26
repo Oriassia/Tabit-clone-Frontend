9e7c387cf6ebc5543adb54a6fd4a37dda1ea3b45
polinakremneva <polinaapolly@gmail.com>
2024-08-26 10:26:16 +0300

    ui fixes fior landing page

--- src/components/costum/ReservationSelector/ReservationSelector.tsx
+++ src/components/costum/ReservationSelector/ReservationSelector.tsx
@@ -29,15 +29,15 @@ export function ReservationSelector({
 }: ReservationSelectorProps) {
   return (
     <div className="flex border-2 rounded-full font-bold font-rubik text-white border-greenButton w-[400px] lg:min-w-[450px] bg-greenBg">
-      <div className="flex flex-col items-center px-[37px] lg:px-[45px] py-[0.5em] lg:text-[19px] border-r-2 border-greenButton">
+      <div className="flex flex-col items-center px-[37px] lg:px-[40px] py-[0.5em] lg:text-[19px] text-[15px] border-r-2 border-greenButton">
         <p className="text-[1em] font-normal">{reservation.dateDay}</p>
-        <p>{reservation.dateDayNumber}</p>
+        <p className="lg:w-[4em]">{reservation.dateDayNumber}</p>
       </div>
       <div className="flex flex-col lg:text-[19px] px-[37px] lg:px-[45px] py-[0.5em] items-center border-r-2 border-greenButton">
         <p className="text-[1em] font-normal">Hour</p>
         <p>{reservation.time}</p>
       </div>
-      <div className="flex flex-col items-center justify-center lg:text-[19px] px-[37px] lg:px-[45px] py-[0.5em]">
+      <div className="flex flex-col items-center justify-center lg:text-[19px] px-[37px] lg:px-[40px] py-[0.5em]">
         <DropdownMenu>
           <DropdownMenuTrigger className="focus:outline-none focus:ring-0">
             <p className="text-[1em] font-normal">Guests</p>
@@ -56,7 +56,7 @@ export function ReservationSelector({
             {[1, 2, 3, 4, 5, 6, 7, 8, 9].map((guestNum) => (
               <DropdownMenuItem
                 key={guestNum}
-                className={`hover:bg-greyHoverDropDownMenu rounded-none cursor-pointer px-4 py-3 ${
+                className={`hover:bg-greyHoverDropDownMenu ocus:outline-none focus:ring-0 hover:border-none rounded-none cursor-pointer px-4 py-3 ${
                   guestNum === reservation.guests
                     ? "bg-greyHoverDropDownMenu"
                     : ""
@@ -74,64 +74,63 @@ export function ReservationSelector({
 }
 
 export function AreaDropdown({
-    area,
-    onAreaChange,
-    onAddNewAddress,
-  }: {
-    area: string;
-    onAreaChange: (newArea: string) => void;
-    onAddNewAddress: () => void;
-  }) {
-    return (
-      <DropdownMenu>
-        <DropdownMenuTrigger className="focus:outline-none focus:ring-0 flex items-center gap-2">
-          <GoDotFill className="text-greenButton text-[19px]" />
-          <span className="font-bold font-rubik text-white text-[19px]">
-            {area}
-          </span>
-          <Button className="border border-greenBorderForIcon bg-transparent dark:hover:bg-transparent dark:bg-transparent p-0 px-[1.5em] h-[2.9em] hover:bg-transparent rounded-full">
-            <MdMyLocation className="size-7 text-white" />
-          </Button>
-        </DropdownMenuTrigger>
-        <DropdownMenuContent
-          className="bg-greyDropDownMenu border-none text-white p-0 rounded-[1%] font-rubik min-w-[250px] max-h-48 overflow-y-auto relative"
-          style={{
-            scrollbarWidth: "none", // Firefox
-            msOverflowStyle: "none", // Internet Explorer and Edge
-          }}
-        >
-          <DropdownMenuItem className="rounded-none select-none">
-            <DropdownMenuLabel className="text-gray-400 font-thin px-[0.6em] py-[0.7em] touch-none">
-              Actual location unavailable
+  area,
+  onAreaChange,
+  onAddNewAddress,
+}: {
+  area: string;
+  onAreaChange: (newArea: string) => void;
+  onAddNewAddress: () => void;
+}) {
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger className="focus:outline-none focus:ring-0 flex items-center gap-2">
+        <GoDotFill className="text-greenButton text-[19px]" />
+        <span className="font-bold font-rubik text-white text-[19px]">
+          {area}
+        </span>
+        <Button className="border border-greenBorderForIcon bg-transparent dark:hover:bg-transparent dark:bg-transparent p-0 px-[1.5em] h-[2.9em] hover:bg-transparent rounded-full">
+          <MdMyLocation className="size-7 text-white" />
+        </Button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent
+        className="bg-greyDropDownMenu border-none text-white p-0 rounded-[1%] font-rubik min-w-[250px] max-h-48 overflow-y-auto relative"
+        style={{
+          scrollbarWidth: "none", // Firefox
+          msOverflowStyle: "none", // Internet Explorer and Edge
+        }}
+      >
+        <DropdownMenuItem className="rounded-none select-none">
+          <DropdownMenuLabel className="text-gray-400 font-thin px-[0.6em] py-[0.7em] touch-none">
+            Actual location unavailable
+          </DropdownMenuLabel>
+        </DropdownMenuItem>
+        {[
+          "Tel Aviv-Jaffa area",
+          "Jerusalem area",
+          "Haifa area",
+          "Center",
+          "North",
+          "South",
+        ].map((location) => (
+          <DropdownMenuItem
+            key={location}
+            className="hover:bg-greyHoverDropDownMenu cursor-pointer px-[0.6em] py-[0.7em]"
+            onClick={() => onAreaChange(location)}
+          >
+            <DropdownMenuLabel className="font-thin">
+              {location}
             </DropdownMenuLabel>
           </DropdownMenuItem>
-          {[
-            "Tel Aviv-Jaffa area",
-            "Jerusalem area",
-            "Haifa area",
-            "Center",
-            "North",
-            "South",
-          ].map((location) => (
-            <DropdownMenuItem
-              key={location}
-              className="hover:bg-greyHoverDropDownMenu cursor-pointer px-[0.6em] py-[0.7em]"
-              onClick={() => onAreaChange(location)}
-            >
-              <DropdownMenuLabel className="font-thin">
-                {location}
-              </DropdownMenuLabel>
-            </DropdownMenuItem>
-          ))}
-          <DropdownMenuItem
-            className="bg-greenButton flex gap-3 justify-center hover:bg-greenButtonDark text-center py-3 font-thin text-[1em] text-white cursor-pointer sticky bottom-0  font-rubik"
-            onClick={onAddNewAddress}
-          >
-            <FaPlus />
-            Add a new address
-          </DropdownMenuItem>
-        </DropdownMenuContent>
-      </DropdownMenu>
-    );
-  }
-  
+        ))}
+        <DropdownMenuItem
+          className="bg-greenButton flex gap-3 justify-center hover:bg-greenButtonDark text-center py-3 font-thin text-[1em] text-white cursor-pointer sticky bottom-0  font-rubik"
+          onClick={onAddNewAddress}
+        >
+          <FaPlus />
+          Add a new address
+        </DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
+}
