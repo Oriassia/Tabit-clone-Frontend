c874dedc474345031bae626756c01c48f55264ef
Ori assia <162992363+Oriassia@users.noreply.github.com>
2024-09-16 11:08:22 +0300

    loaders

--- src/services/api.services.ts
+++ src/services/api.services.ts
@@ -1,12 +1,12 @@
 import axios from "axios";
 
 const baseURLHolder =
-  import.meta.env.VITE_ENV === "development"
+  import.meta.env.VITE_ENV === "development" // Use Vite's `import.meta.env`
     ? import.meta.env.VITE_DEV_API_BASE_URL
     : import.meta.env.VITE_PROD_API_BASE_URL;
 
 const api = axios.create({
-  baseURL: baseURLHolder, // Use Vite's `import.meta.env`
+  baseURL: baseURLHolder,
   withCredentials: true,
 });
 
